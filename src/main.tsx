439130c7cbe239854b669ae98d6042e9505aa78b
tomori-k <108829509+tomori-k@users.noreply.github.com>
2025-06-02 03:11:30 +0900

    Add internationalization support with i18next and language selection settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import './i18n' // i18nの初期化を行うためにインポート
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
